53599c74c738d0e94c7fb14062a397af5f77fbd8
Siiickzer <suphanath_w@cmu.ac.th>
2022-02-18 21:37:03 +0700

    Fix and ad return

--- src/extraCalculator/Vector.cpp
+++ src/extraCalculator/Vector.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-void sub(){
+double sub(){
     double x1,y1,z1;
     double x2,y2,z2;
     double x3,y3,z3;
@@ -32,10 +32,10 @@ void sub(){
     cout << "(" << x2 << " " << y2 << " " << z2 << ")";
     cout << " = ";
     cout << "(" << x3 << " " << y3 << " " << z3 << ")";
+    return x3,y3,z3;
 }
 
-
-void mul(){
+double mul(){
     double num;
     double x1,y1,z1;
     double x2,y2,z2;
@@ -61,20 +61,26 @@ void mul(){
         z2 = z1/num;
     }
     cout << num << " = (" << x2 << " " << y2 << " " << z2 << ")"; ;
+    return x2,y2,z2;
 }
 
-double dot(double x1,double y1, double z1, double x2,double y2, double z2){
+double dot(){
+    double x1,y1,z1,x2,y2,z2;
     double result;
+    cout << "input (x1 y1 z1): ";
+    cin >> x1 >> y1 >> z1;
+    cout << "input (x2 y2 z2): ";
+    cin >> x2 >> y2 >> z2;
     result = (x1*x2)+(y1*y2)+(z1*z2);
     cout << "(" << x1 << " " << y1 << " " << z1 << ")";
     cout << ".";
     cout << "(" << x2 << " " << y2 << " " << z2 << ")";
     cout << " = ";
     cout << result;
-    return 0;
+    return result;
 }
 
-void cross(){
+double cross(){
     double a,b,c;
     double d,e,f;
     double result1;
@@ -93,20 +99,25 @@ void cross(){
     cout << "| i j k |\n";
     cout << "| " << a << " " << b << " " << c << " | =   (" << result1 << ")i+(" << result2 << ")j+(" << result3 << ")k\n";
     cout << "| " << d << " " << e << " " << f << " |";
-
+    return result1, result2, result3;
 }
 
-double mag(double a, double b, double c){
+double mag(){
+    double x,y,z;
     double result;
-    result = sqrt(pow(a,2)+pow(b,2)+pow(c,2));
-    cout << "| "<< a << " " << b << " " << c << " | = " << result;
+    cout << "input | x y z |: \n";
+    cin >> x >> y >> z;
+    result = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+    cout << "| "<< x << " " << y << " " << z << " | = " << result;
     return result;
 }
-void ang(){
+
+double ang(){
     double result1;
     double result2;
     double result3;
     double result4;
+    double result5;
     double x1,y1,z1;
     double x2,y2,z2;
     cout << "input (x1 y1 z1): \n";
@@ -117,29 +128,42 @@ void ang(){
     result2 = sqrt(pow(x1,2)+pow(y1,2)+pow(z1,2));
     result3 = sqrt(pow(x2,2)+pow(y2,2)+pow(z2,2));
     result4 = acos((result1)/(result2*result3));
-    cout << result4/(M_PI / 180) << " degrees";
+    result5 = result4/(M_PI / 180);
+    cout << result5 << " degrees";
+    return result5;
+
 }
 
-void unit(){
+double unit(){
     double x1,y1,z1;
+    double x2,y2,z2;
     double result;
     cout << "input (x1 y1 z1): \n";
     cin >> x1 >> y1 >> z1;
-    result = mag(x1,y1,z1);
+    result = sqrt(pow(x1,2)+pow(y1,2)+pow(z1,2));
     cout << endl;
-    cout << "( " << x1 << "/" << result << " " << y1 << "/" << result << " " << z1 << "/" << result << " )" ;
+    cout << "( " << x1 << "/" << result << " " << y1 << "/" << result << " " << z1 << "/" << result << " )";
+    x2 =  x1/result;
+    y2 =  y1/result;
+    z2 =  z1/result;
+    double x2,y2,z2;
 }
 
-double proj(double x1,double y1,double z1,double x2,double y2,double z2){
+double proj(){
+    double x1,y1,z1,x2,y2,z2;
     double result1, result2, result3;
+    cout << "input v=(x1 y1 z1): ";
+    cin >> x1 >> y1 >> z1;
+    cout << "input u=(x2 y2 z2): ";
+    cin >> x2 >> y2 >> z2;
+    cout << "proj u(v) =";
     result1 = ((x1*x2)+(y1*y2)+(z1*z2))/(pow(sqrt(pow(x2,2)+pow(y2,2)+pow(z2,2)),2))*x2;
     result2 = ((x1*x2)+(y1*y2)+(z1*z2))/(pow(sqrt(pow(x2,2)+pow(y2,2)+pow(z2,2)),2))*y2;
     result3 = ((x1*x2)+(y1*y2)+(z1*z2))/(pow(sqrt(pow(x2,2)+pow(y2,2)+pow(z2,2)),2))*z2;
     cout << "(" << result1 << " " << result2 << " " << result3 << ")";
-    return 0;
+    return result1, result2, result3;
 }
 
 int main(){
-    unit();
     return 0;
 }
