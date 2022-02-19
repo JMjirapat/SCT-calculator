c65a248732abdc242cecf7d503553400b07c0738
Zeronos <boss16805@gmail.com>
2022-02-19 14:59:48 +0700

    Add Linear Equation solver

---
+++ include/extraCalculator/LE.h
@@ -0,0 +1,9 @@
+#include "../../src/extraCalculator/LE.cpp"
+
+double determinant(double, int);
+
+void shiftdet(double,double,double,int);
+
+void solve(double,double,double,int);
+
+void LE();
---
+++ test/extraCalculator/LEtest.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "../../include/extraCalculator/LE.h"
+int main(){ // Linear equation main function
+   LE();
+   return 0;
+}
